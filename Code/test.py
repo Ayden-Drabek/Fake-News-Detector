239b8f804d0dfe76d2f4c92d207593be8e34d63a
Drabek <Ayden.Drabek@mines.sdsmt.edu>
2020-03-10 13:50:09 +0100

    data at end is the average vector for an article

--- Code/test.py
+++ Code/test.py
@@ -7,26 +7,33 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-#import xgboost as xgb
+from gensim.models import Word2Vec, KeyedVectors
+from adjustText import adjust_text
 from sklearn.manifold import TSNE
+
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, confusion_matrix
-from gensim.models import Word2Vec, KeyedVectors
-from adjustText import adjust_text
+
 
 
 def read_dataset(file,sheet,column1):
 	df = pd.read_excel(str(file), sheet_name=str(sheet))
 	URL = df[column1]
-	return URL
+	Body = df['Body']
+	label = df['Label']
+	return URL, Body, label
 
 
 def read_article(URL):
-	print(URL)
 	scrapped_data = urllib.request.urlopen(URL)
 	article = scrapped_data .read()
 
+	all_words = clean_article(article)
+	return all_words
+
+
+def clean_article(article):
 	parsed_article = bs.BeautifulSoup(article,'lxml')
 
 	paragraphs = parsed_article.find_all('p')
@@ -68,7 +75,7 @@ def word_to_vec(all_words):
 	return df
 
 def tsne(df):
-		# Initialize t-SNE
+	# Initialize t-SNE
 	tsne = TSNE(n_components = 2, init = 'random', random_state = 10, perplexity = 100)
 
 	# Use only 400 rows to shorten processing time
@@ -80,12 +87,10 @@ def tsne(df):
 	# Import adjustText, initialize list of texts
 	texts = []
 	words_to_plot = list(np.arange(0, len(tsne_df), 10))
-	#print (words_to_plot)
 
 	# Append words to list
 	for word in words_to_plot:
 		texts.append(plt.text(tsne_df[word, 0], tsne_df[word, 1], df.index[word], fontsize = 14))
-		#print(texts)
 	# Plot text using adjust_text (because overlapping text is hard to read)
 	adjust_text(texts, force_points = 0.4, force_text = 0.4, 
 	            expand_points = (2,1), expand_text = (1,2),
@@ -99,40 +104,21 @@ vocab = model.vocab.keys()
 file = sys.argv[1]
 sheet = sys.argv[2]
 column = sys.argv[3]
-URL= read_dataset(file,sheet,column)
+URL, body, label = read_dataset(file,sheet,column)
 
-file1 = open("myfile.txt","w") 
 avg = np.arange(300, dtype=np.float16)
-sums = np.arange(300, dtype=np.float16)
+data = []
 
-
-
-for i in range(10):
+for i in range(245):
 	data = []
-	new_sums = []
-	print(str(i))
-	all_words = read_article(URL[i])
-	#print(all_words)
+	#print(str(i))
+	all_words = clean_article(body[i])
 	df = word_to_vec(all_words)
 	for k in range(300):
 		avg[k] = np.mean(df[k])
-		sums[k] = np.sum(np.absolute(df[k]))
-		data.append(avg)
-		new_sums.append(sums)
 
-
-	data = np.asarray(data)
-	new_sums = np.asarray(new_sums)
-	print(data)
-	print("\n\n")
-	print(new_sums)
-
-#SVM(data)
+	#the average vector for each article 
+	data.append(avg)
 	#tsne(df)
 
-	#print(all_words)
-	#word2vec = Word2Vec(all_words, min_count=2,size=30)
-	#print(word2vec)
-	#vocabulary = word2vec.wv.vocab
-	#print(vocabulary)
-#plt.show()
+plt.show()
